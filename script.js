ab2c10aad9d01c26083d940371fe99476e7d3814
alefwhite <alefwhite@hotmail.com>
2022-03-14 13:57:35 -0300

    feat: Incluindo timer aleatorio

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const result = document.querySelector(".result > p");
-const draw = document.querySelector("#draw")
-const award = document.querySelector("#award")
 
-//result.innerHTML = 10
+const draw = document.querySelector("#draw");
+const award = document.querySelector("#award");
+const clean = document.querySelector("#clean");
+
+let timer;
+
 
 function addValue (value) {
     result.innerHTML = value
 }
 
+function generateValue () {
+    timer = setInterval(() => {
+        const value = Math.ceil(Math.random() * 100)
+        addValue(value)
+    }, 10)
+}
+
 
 award.addEventListener('click', function () {
-    addValue(0)
+   clearInterval(timer)
 })
 
 draw.addEventListener('click', function () {
-    const value = Math.ceil(Math.random() * 100)
-    addValue(value)
+    generateValue()
+})
+
+clean.addEventListener('click', function () {
+    result.innerHTML = 0
 })
 
